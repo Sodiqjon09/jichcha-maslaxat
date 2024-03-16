edd7819e08f8b85c0ad4305c16ce8e4b81ad6058
Sodiqjon Sharipov <134689903+Sodiqjon09@users.noreply.github.com>
2024-03-16 13:31:24 +0500

    useInfiniteQuery loading more

--- src/components/booksKitoblar/alMaxtumPageMain/alMaxtumPageMain.jsx
+++ src/components/booksKitoblar/alMaxtumPageMain/alMaxtumPageMain.jsx
@@ -1,5 +1,4 @@
-// .jsx
-
+import { useState } from "react";
 import { useInfiniteQuery } from "react-query";
 
 const posts = [
@@ -9,6 +8,12 @@ const posts = [
   { id: 4, title: "post 4" },
   { id: 5, title: "post 5" },
   { id: 6, title: "post 6" },
+  { id: 7, title: "post 7" },
+  { id: 8, title: "post 8" },
+  { id: 9, title: "post 9" },
+  { id: 10, title: "post 10" },
+  { id: 11, title: "post 11" },
+  { id: 12, title: "post 12" },
 ];
 
 const fetchPost = async (page) => {
@@ -17,6 +22,9 @@ const fetchPost = async (page) => {
 };
 
 export default function AlMaxtumPageMain() {
+  const [titleData, setTitleData] = useState(true);
+  const [textData, setTextData] = useState(false);
+
   const { data, fetchNextPage, isFetchingNextPage } = useInfiniteQuery(
     "query",
     async ({ pageParam = 1 }) => {
@@ -34,28 +42,38 @@ export default function AlMaxtumPageMain() {
     }
   );
 
+  const loadMore = () => {
+    setTextData(true);
+    setTitleData(false);
+    setTimeout(() => {
+      setTitleData(true);
+      setTextData(false);
+    }, 2000);
+    fetchNextPage();
+  };
+
   return (
     <div style={{ marginTop: "50px" }} className="px-[60px]" id="sections">
       {data?.pages.map((page, i) => (
         <div key={i}>
           {page.map((post) => (
-            <div id="slides" key={post.id} className="text-[#fff]">
-              {post.title}
+            <div key={post.id} id="slides" className="post text-[#fff]">
+              {titleData && <h1>{post.title}</h1>}
+              {textData && <h1>Loading...</h1>}
             </div>
           ))}
         </div>
       ))}
-      <button onClick={() => fetchNextPage()} disabled={isFetchingNextPage}>
+      <button onClick={loadMore} disabled={isFetchingNextPage}>
         {isFetchingNextPage ? (
-          <h1 className="text-[20px] text-[#fff] fixed bottom-0 top-50">
-            Loading more..
-          </h1>
-        ) : data?.pages.length < 3 ? (
-          <h1 className="text-[20px] text-[#fff] fixed right-12 top-[660px] hover:text-[green] hover:underline">
-            Loading more
-          </h1>
+          <h1
+            id="text"
+            className="text-[20px] text-[#fff] fixed bottom-0 top-50"
+          ></h1>
         ) : (
-          <h1 className="text-[red]">Nothing more to load</h1>
+          <h1 className="text-[20px] text-[#fff] fixed right-12 top-[620px] hover:text-[green] hover:underline">
+            Load More
+          </h1>
         )}
       </button>
     </div>
